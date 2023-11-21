fe2109868738b9f6e2fa0d3dbceaceab0670ab9d
gilbertk27 <64368640+gilbertk27@users.noreply.github.com>
2023-11-21 18:05:17 +0700

    update submission ngc 5 & 6

--- Phase-0/Week-2/NGC/ngc5_query.sql
+++ Phase-0/Week-2/NGC/ngc5_query.sql
@@ -1,2 +1,3 @@
-SELECT customer_name, total_order 
-WHERE
+SELECT customer_name, count(customer_name) as total_order
+FROM Orders JOIN Customers ON Orders.customer_id = Customers.customer_id
+GROUP BY customer_name
